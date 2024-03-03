4d21a5fdd062d67f7a998ad0968b5f27c795f177
Ahmed Afifi Abdou <aafifi1988@icloud.com>
2024-03-03 21:32:30 +0200

    -Update

--- Assets/Project/Scripts/Collectable/Collectable Manager.cs
+++ Assets/Project/Scripts/Collectable/Collectable Manager.cs
@@ -3,10 +3,6 @@ using UnityEngine.UI;
 
 public class CollectableManager : MonoBehaviour
 {
-    [SerializeField] private Image dropletWater;
-    [SerializeField] private Image fertilizer;
-    [SerializeField] private Image sunlight;
-
     internal int _dropletWaterCounter = 0;
     internal int _fertilizerCounter = 0;
     internal int _sunlightCounter = 0;
