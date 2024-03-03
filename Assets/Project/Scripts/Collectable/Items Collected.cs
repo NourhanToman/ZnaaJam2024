16c3a459207ef2e63182bec8bca41cda4fa7bd32
Ahmed Afifi Abdou <aafifi1988@icloud.com>
2024-03-03 13:32:47 +0200

    -Update

--- Assets/Project/Scripts/Collectable/Items Collected.cs
+++ Assets/Project/Scripts/Collectable/Items Collected.cs
@@ -1,16 +1,37 @@
+using System.Collections;
 using UnityEngine;
 
 public class ItemsCollected : MonoBehaviour
 {
     [SerializeField] private GameEvents Event;
+    [SerializeField] private float fadeOutDuration = 1f;
 
-    private void OnCollisionExit2D(Collision2D other)
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake() => spriteRenderer = GetComponent<SpriteRenderer>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (other.gameObject.CompareTag(GameConstant.PlayerTag))
+        if (collision.gameObject.CompareTag(GameConstant.PlayerTag))
         {
             Event.GameAction?.Invoke();
-            Destroy(gameObject);
-            Debug.Log("collected");
+            StartCoroutine(FadeOutAndDestroy());
         }
+    }
+
+    private IEnumerator FadeOutAndDestroy()
+    {
+        float startTime = Time.time;
+
+        while (Time.time - startTime < fadeOutDuration)
+        {
+            float t = (Time.time - startTime) / fadeOutDuration;
+            Color color = spriteRenderer.color;
+            color.a = Mathf.Lerp(1, 0, t);
+            spriteRenderer.color = color;
+            yield return null;
+        }
+
+        Destroy(gameObject);
     }
 }
